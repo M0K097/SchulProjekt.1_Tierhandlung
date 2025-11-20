4d2d958f3d87b55cba3c99ad50fd900b0c676aa3
Moritz Kolb <moritz.kolb@digitale-schule.saarland>
2025-11-20 11:20:37 +0100

    implemented Tier class and Tierheim class

---
+++ Implementierung/Tierhandlung_Projekt.1_Moritz_Kolb/Tier.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tierhandlung_Projekt._1_Moritz_Kolb
+{   
+    public enum tier_art
+    {
+        Hund,
+        Katze,
+        Hamster,
+        Ratte,
+        Maus,
+        Vogel,
+        Reptiel
+    }
+
+    public static class Tierheim
+    {
+        static List<Tier> alle_tiere = new ();
+
+    }
+    public class Tier
+    {
+        public tier_art tierart { get; set; }
+        public string name { get; set; }
+        public DateTime geburtsdatum { get; set; }
+        public string beschreibung { get; set; }
+        public bool reserviert { get; set; }
+
+        public Tier(tier_art art, string name_des_tieres, DateTime geburtsdatum)
+        {
+            tierart = art;
+            name = name_des_tieres;
+            this.geburtsdatum = geburtsdatum;
+            beschreibung = "keine Beschreibung";
+            reserviert = false;
+            
+        }
+            
+    }
+}
